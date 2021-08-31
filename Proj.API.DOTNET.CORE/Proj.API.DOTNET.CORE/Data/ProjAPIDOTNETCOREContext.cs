21472c1e1e92fcd9fe159df0faa851a5c408eeb2
caiomotta4 <62080324+caiomotta4@users.noreply.github.com>
2021-08-30 22:04:57 -0300

    .

---
+++ Proj.API.DOTNET.CORE/Proj.API.DOTNET.CORE/Data/ProjAPIDOTNETCOREContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Proj.API.DOTNET.CORE.Model;
+
+namespace Proj.API.DOTNET.CORE.Data
+{
+    public class ProjAPIDOTNETCOREContext : DbContext
+    {
+        public ProjAPIDOTNETCOREContext (DbContextOptions<ProjAPIDOTNETCOREContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Proj.API.DOTNET.CORE.Model.Person> Person { get; set; }
+    }
+}
---
+++ Proj.API.DOTNET.CORE/Proj.API.DOTNET.CORE/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proj.API.DOTNET.CORE.Model
+{
+    public class Person
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+
+        #endregion
+    }
+}
