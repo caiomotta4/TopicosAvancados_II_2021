1f7cbd99ccfb96c2fbbb1b6b4b4fad02b7e84ea7
caiomotta4 <62080324+caiomotta4@users.noreply.github.com>
2021-09-20 20:23:42 -0300

    .

---
+++ SolutionCine/ProjCineWeb/Controllers/IngressoesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjCineWeb.Data;
+using ProjCineWeb.Model;
+
+namespace ProjCineWeb.Controllers
+{
+    public class IngressoesController : Controller
+    {
+        private readonly ProjCineWebContext _context;
+
+        public IngressoesController(ProjCineWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ingressoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ingresso.ToListAsync());
+        }
+
+        // GET: Ingressoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingresso = await _context.Ingresso
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingresso == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingresso);
+        }
+
+        // GET: Ingressoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ingressoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Valor,DataHora")] Ingresso ingresso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingresso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingresso);
+        }
+
+        // GET: Ingressoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingresso = await _context.Ingresso.FindAsync(id);
+            if (ingresso == null)
+            {
+                return NotFound();
+            }
+            return View(ingresso);
+        }
+
+        // POST: Ingressoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Valor,DataHora")] Ingresso ingresso)
+        {
+            if (id != ingresso.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingresso);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngressoExists(ingresso.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingresso);
+        }
+
+        // GET: Ingressoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingresso = await _context.Ingresso
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingresso == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingresso);
+        }
+
+        // POST: Ingressoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingresso = await _context.Ingresso.FindAsync(id);
+            _context.Ingresso.Remove(ingresso);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IngressoExists(int id)
+        {
+            return _context.Ingresso.Any(e => e.Id == id);
+        }
+    }
+}
